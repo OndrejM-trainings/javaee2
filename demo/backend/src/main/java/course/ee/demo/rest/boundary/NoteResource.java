e0d04eabd6fc113a0007479f413ba13095de8565
Ondro Mihalyi <ondrej.mihalyi@gmail.com>
2023-06-06 06:14:29 +0200

    Demo: Add support for XML media type Signed-off-by:Ondro Mihalyi <ondrej.mihalyi@gmail.com>

--- demo/backend/src/main/java/course/ee/demo/rest/boundary/NoteResource.java
+++ demo/backend/src/main/java/course/ee/demo/rest/boundary/NoteResource.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 
 @Path("note")
 @RequestScoped
-@Produces(MediaType.APPLICATION_JSON)
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public class NoteResource {
 
     @Inject
