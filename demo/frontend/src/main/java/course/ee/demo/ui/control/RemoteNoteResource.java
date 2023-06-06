398ec0b443daee324a1d9be4176a0bf36b30646d
Ondro Mihalyi <ondrej.mihalyi@gmail.com>
2023-06-06 06:34:06 +0200

    Demo: MP REST client alternative Signed-off-by:Ondro Mihalyi <ondrej.mihalyi@gmail.com>

--- demo/frontend/src/main/java/course/ee/demo/ui/control/RemoteNoteResource.java
+++ demo/frontend/src/main/java/course/ee/demo/ui/control/RemoteNoteResource.java
@@ -6,12 +6,33 @@ import course.ee.demo.core.entity.Note;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.GenericType;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 @ApplicationScoped
 public class RemoteNoteResource {
 
     public Collection<Note> getAll() {
+        return getAllMP();
+    }
+
+    private Collection<Note> getAllMP() {
+        return getNoteResource().getAll();
+    }
+
+    private NoteResource getNoteResource() {
+        try {
+            return RestClientBuilder.newBuilder()
+                    .baseUri(new URI("http://localhost:8080/demo/rest"))
+                    .build(NoteResource.class);
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Collection<Note> getAllEE() {
         return ClientBuilder.newClient().target("http://localhost:8080/demo/rest/note")
                 .request(APPLICATION_JSON)
                 .get(new GenericType<Collection<Note>>() {
